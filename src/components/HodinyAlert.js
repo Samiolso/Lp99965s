301fded6f6ce7f1d81f77c7ba4d424bcb9f725d4
Pizza Lucky <pizzerialucky7@gmail.com>
2020-03-09 08:42:46 +0100

    +

--- src/components/HodinyAlert.js
+++ src/components/HodinyAlert.js
@@ -5,7 +5,7 @@ import { Alert } from "./alert"
 export const HodinyAlert = () => {
   const hours = new Date().getHours()
 
-  const hoursWeek = (hours >= 8 && hours <= 21)
+  const hoursWeek = (hours >= 10 && hours <= 21)
 
   return (
     <div>
